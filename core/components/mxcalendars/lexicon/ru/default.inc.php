7d933493e7416d5b15905201c6a257c9a31c3e28
Dmitry Danilson <patchranger@gmail.com>
2015-05-25 14:09:37 +0600

    Lexicon (ru)

---
+++ core/components/mxcalendars/lexicon/ru/default.inc.php
@@ -0,0 +1,200 @@
+<?php
+$_lang['mxcalendars'] = 'mxCalendar';
+$_lang['mxcalendars.desc'] = 'Управление календарём.';
+$_lang['mxcalendars.name'] = 'Название';
+$_lang['mxcalendars.description'] = 'Описание';
+$_lang['mxcalendars.management'] = 'Управление mxCalendar';
+$_lang['mxcalendars.management_desc'] = 'Управление календарём mxCalendar. Двойной щелчок - редактирование, щелчок правой для меню дополнительных опций.';
+$_lang['mxcalendars.event_title_create'] = 'Создать новое событие';
+$_lang['mxcalendars.event_title_update'] = 'Изменить событие';
+$_lang['mxcalendars.event_title_duplicate'] = 'Клонировать событие';
+$_lang['mxcalendars.event_title_remove'] = 'Удалить событие';
+$_lang['mxcalendars.tab_events'] = 'События';
+$_lang['mxcalendars.tab_categories'] = 'Категории';
+$_lang['mxcalendars.tab_settings'] = 'Глобальные настройки';
+$_lang['mxcalendars.tab_calendars'] = 'Календари';
+$_lang['mxcalendars.grid_col_context'] = 'Контекст';
+$_lang['mxcalendars.grid_col_title'] = 'Название';
+$_lang['mxcalendars.grid_col_category'] = 'Категория';
+$_lang['mxcalendars.grid_col_start'] = 'Дата и время начала';
+$_lang['mxcalendars.grid_col_end'] = 'Дата и время конца';
+$_lang['mxcalendars.grid_col_repeate'] = 'Повторяющееся';
+$_lang['mxcalendars.grid_col_formchunk'] = 'Форма';
+$_lang['mxcalendars.txt_search_default'] = 'Искать ...';
+$_lang['mxcalendars.btn_display_past_events'] = 'Показать прошедшие события';
+$_lang['mxcalendars.btn_create'] = 'Создать новое событие календаря';
+$_lang['mxcalendars.label_product'] = 'Продукт';
+$_lang['mxcalendars.label_industry'] = 'Отрасль';
+$_lang['mxcalendars.label_select_context'] = 'Выберите контекст (пустое - все)';
+$_lang['mxcalendars.label_select_calendar'] = 'Выберите календарь';
+$_lang['mxcalendars.label_today'] = 'сегодня';
+$_lang['mxcalendars.label_repeating_event'] = 'Повторяющееся событие';
+$_lang['mxcalendars.label_repeat_type'] = 'Тип повторения';
+$_lang['mxcalendars.label_repeaton']='Повторять по';
+$_lang['mxcalendars.label_repeat_frequency'] = 'Частота повторений';
+$_lang['mxcalendars.label_repeat_last_occurance'] = 'Повторять до';
+$_lang['mxcalendars.label_description'] = 'Описание';
+$_lang['mxcalendars.label_location'] = 'Местоположение';
+$_lang['mxcalendars.label_name'] = 'Название';
+$_lang['mxcalendars.label_address'] = 'Адрес';
+$_lang['mxcalendars.label_display_map'] = 'Показывать карту';
+$_lang['mxcalendars.label_link'] = 'Ссылка';
+$_lang['mxcalendars.label_link_rel'] = 'Rel ссылки';
+$_lang['mxcalendars.label_link_target'] = 'Target ссылки';
+$_lang['mxcalendars.label_cancel'] = 'Отмена';
+$_lang['mxcalendars.label_save'] = 'Сохранить';
+$_lang['mxcalendars.label_sunday'] = 'Воскресенье';
+$_lang['mxcalendars.label_monday'] = 'Понедельник';
+$_lang['mxcalendars.label_tuesday'] = 'Вторник';
+$_lang['mxcalendars.label_wednesday'] = 'Среда';
+$_lang['mxcalendars.label_thursday'] = 'Четверг';
+$_lang['mxcalendars.label_friday'] = 'Пятница';
+$_lang['mxcalendars.label_saturday'] = 'Суббота';
+$_lang['mxcalendars.label_dow_7'] = 'вс';
+$_lang['mxcalendars.label_dow_1'] = 'пн';
+$_lang['mxcalendars.label_dow_2'] = 'вт';
+$_lang['mxcalendars.label_dow_3'] = 'ср';
+$_lang['mxcalendars.label_dow_4'] = 'чт';
+$_lang['mxcalendars.label_dow_5'] = 'пт';
+$_lang['mxcalendars.label_dow_6'] = 'сб';
+$_lang['mxcalendars.label_allday'] = 'Весь день';
+$_lang['mxcalendars.label_daily'] = 'Каждый день';
+$_lang['mxcalendars.label_weekly'] = 'Каждую неделю';
+$_lang['mxcalendars.label_monthly'] = 'Каждый месяц';
+$_lang['mxcalendars.label_yearly'] = 'Каждый год';
+$_lang['mxcalendars.label_forms'] = 'Формы';
+$_lang['mxcalendars.label_images'] = 'Изображения';
+$_lang['mxcalendars.label_select_image'] = 'Изображение';
+$_lang['mxcalendars.btn_create_image'] = 'Добавить изображение';
+$_lang['mxcalendars.image_filepath'] = 'Путь к файлу';
+$_lang['mxcalendars.label_videos'] = 'Видео';
+$_lang['mxcalendars.label_select_video'] = 'Видео';
+$_lang['mxcalendars.btn_create_video'] = 'Добавить видео';
+$_lang['mxcalendars.video_filepath'] = 'Путь к файлу';
+$_lang['mxcalendars.label_create'] = 'Создать';
+$_lang['mxcalendars.menu_update'] = 'Изменить';
+$_lang['mxcalendars.menu_remove'] = 'Удалить';
+$_lang['mxcalendars.default_search'] = 'Искать ...';
+$_lang['mxcalendars.err_ae'] = 'Название занято другим событием.';
+$_lang['mxcalendars.err_nf'] = 'Поле "id" обязательно.';
+$_lang['mxcalendars.err_ns'] = 'Не найдена запись для изменения.';
+$_lang['mxcalendars.err_ns_name'] = 'Пожалуйста, укажите Название для события.';
+$_lang['mxcalendars.err_ns_title'] = 'Пожалуйста, укажите Название для события.';
+$_lang['mxcalendars.err_remove'] = 'Во время удаления события произошла ошибка.';
+$_lang['mxcalendars.err_pre_save'] = 'Исправьте ошибки до сохранения.';
+$_lang['mxcalendars.err_save'] = 'Во время сохранения события произошла ошибка.';
+$_lang['mxcalendars.err_event_req_name'] = 'Поле "Название" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_category'] = 'Поле "Категория" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_startdate'] = 'Поле "Дата начала" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_starttime'] = 'Поле "Время начала" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_enddate'] = 'Поле "Дата конца" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_endtime'] = 'Поле "Время конца" обязательно для заполнения.';
+$_lang['mxcalendars.err_event_req_context'] = 'Поле "Контекст" обязательно для заполнения.';
+$_lang['mxcalendars.label_select_context_err'] = 'Все сайты (контексты)';
+$_lang['mxcalendars.err_event_req_repeattype'] = 'Поле "Тип повторения" обязательно для заполнения для повторяющихся событий.';
+$_lang['mxcalendars.err_event_req_repeatfrequency'] = 'Поле "Частота повторения" обязательно для заполнения для повторяющихся событий.';
+$_lang['mxcalendars.err_event_req_repeatenddate'] = 'Поле "Повторять до" обязательно для заполнения для повторяющихся событий.';
+$_lang['mxcalendars.err_event_req_repeaton'] = 'Поле "Повторять по" обязательно для заполнения для повторяющихся каждую неделю событий.';
+$_lang['mxcalendars.err_event_req_validcat'] = 'Не найдено умолчательной категории. Пожалуйста, убедитесь, что задана умолчательная категория или задайте категорию.';
+$_lang['mxcalendars.notice_no_events'] = 'Событий не найдено.';
+
+$_lang['mxcalendars.mxcalendars_remove_confirm'] = 'Вы уверены, что хотите удалить это событие?';
+$_lang['mxcalendars.downloads'] = 'Загрузки';
+$_lang['mxcalendars.location'] = 'Местоположение';
+$_lang['mxcalendars.search_default_text'] = 'Искать по событиям...';
+$_lang['mxcalendars.top_downloaded'] = 'Наиболее скачиваемые события';
+$_lang['mxcalendars.categoryid_col_label'] = 'Категория';
+$_lang['mxcalendars.startdate_col_label'] = 'Дата начала';
+$_lang['mxcalendars.starttime_col_label'] = 'Время начала';
+$_lang['mxcalendars.enddate_col_label'] = 'Дата конца';
+$_lang['mxcalendars.endtime_col_label'] = 'Время конца';
+$_lang['mxcalendars.repeating_col_label'] = 'Повторяющееся';
+$_lang['mxcalendars.repeating_last_occ_col_label'] = 'Повторять до';
+
+$_lang['mxcalendars.label_startdate'] = 'Дата и время начала';
+$_lang['mxcalendars.label_starttime'] = 'Время начала';
+$_lang['mxcalendars.label_title'] = 'Название';
+$_lang['mxcalendars.label_enddate'] = 'Дата и время конца';
+$_lang['mxcalendars.label_duplicate'] = 'Клонировать';
+$_lang['mxcalendars.label_select_category'] = 'Выберите категорию';
+$_lang['mxcalendars.label_select_category_err'] = 'Выберите существующую категорию';
+$_lang['mxcalendars.label_calendar_id'] = 'ID календаря';
+
+$_lang['mxcalendars.label_select_form'] = 'Выберите форму';
+$_lang['mxcalendars.label_select_form_err'] = 'Выберите существующий чанк формы';
+
+$_lang['mxcalendars.btn_show_past_events'] = 'Показать прошедшие события';
+$_lang['mxcalendars.btn_show_upcoming_events'] = 'Показать предстоящие события';
+$_lang['mxcalendars.tip_repeaton'] = 'Повторять каждые ___ (дней|недель|месяцев|лет).<br /><br />Задайте соответствующую типу частоту повторений';
+
+$_lang['mxcalendars.label_content'] = 'Контент';
+
+//$_lang['mxcalendars.'] = '';
+
+//-- Category Management
+$_lang['mxcalendars.category_desc'] = 'Управление категориями событий. Каждая категория может иметь собственные цвета.';
+$_lang['mxcalendars.label_window_create'] = 'Создать новую категорию';
+$_lang['mxcalendars.category_name_col_label'] = 'Название';
+$_lang['mxcalendars.category_isdefault_col_label'] = 'Является категорией по умолчанию';
+$_lang['mxcalendars.category_foregroundcss_col_label'] = 'Цвет текста';
+$_lang['mxcalendars.category_backgroundcss_col_label'] = 'Цвет фона';
+$_lang['mxcalendars.category_inlinecss_col_label'] = 'Inline CSS';
+$_lang['mxcalendars.category_disabled_col_label'] = 'Отключена';
+$_lang['mxcalendars.category_active_col_label'] = 'Активна';
+$_lang['mxcalendars.cateogry_remove_title'] = 'Удалить категорию';
+$_lang['mxcalendars.cateogry_remove_confirm'] = 'Вы уверены, что хотите удалить эту категорию?';
+$_lang['mxcalendars.btn_create_cat'] = 'Создать категорию';
+$_lang['mxcalendars.default_category_search'] = "Искать по категориям...";
+$_lang['mxcalendars.menu_update_category'] = 'Изменить категорию';
+$_lang['mxcalendars.menu_remove_category'] = 'Удалить категорию';
+
+$_lang['mxcalendars.label_category_heading'] = 'Категории:';
+$_lang['mxcalendars.label_category_viewAll'] = 'Все';
+
+//-- Settings friendly names
+$_lang['mxcalendars.set_category_required'] = 'Category required for event';
+$_lang['mxcalendars.set_event_desc_type'] = 'Description field type';
+$_lang['mxcalendars.set_mgr_dateformat'] = 'Формат даты';
+$_lang['mxcalendars.set_mgr_timeformat'] = 'Формат времени';
+
+//-- Calendar Management
+$_lang['mxcalendars.calendar_desc'] = 'Управляйте календарями событий.';
+
+$_lang['mxcalendars.err_ac'] = 'При сохранении календаря возникла ошибка. Пожалуйста , попробуйте позже.';
+$_lang['mxcalendars.calendar_name_col_label'] = 'Название';
+$_lang['mxcalendars.calendar_name_col_active'] = 'Активен';
+$_lang['mxcalendars.calendar_default_calendar_search'] = 'Искать...';
+$_lang['mxcalendars.calendar_btn_create'] = 'Создать календарь';
+
+
+$_lang['mxcalendars.calendar_context_menu_update'] = 'Изменить';
+$_lang['mxcalendars.calendar_context_menu_remove'] = 'Удалить';
+
+$_lang['mxcalendars.label_select_calendar'] = 'Выберите календарь';
+$_lang['mxcalendars.label_select_calendar_err'] = 'Календарь не найден';
+$_lang['mxcalendars.grid_col_calendar'] = 'Календарь';
+
+//-- Feed lexicon entries
+$_lang['mxcalendars.tab_feed'] = 'Ленты';
+$_lang['mxcalendars.feed_desc'] = 'Введите публично доступную ссылку на iCalendar или XML ленту.';
+
+$_lang['mxcalendars.default_feed_search'] = 'Искать по лентам';
+$_lang['mxcalendars.feed_btn_create'] = 'Создать ленту';
+$_lang['mxcalendars.feed_menu_update'] = 'Изменить ленту';
+$_lang['mxcalendars.feed_menu_remove'] = 'Удалить ленту';
+$_lang['mxcalendars.feed_remove_title'] = 'Подтверждение удаления ленты';
+$_lang['mxcalendars.feed_remove_confirm'] = 'Это полностью удалит ленту. В будущем импорт из неё не будет осуществляться.';
+$_lang['mxcalendars.feed_col_feed'] = 'Ссылка на ленту';
+$_lang['mxcalendars.feed_col_active'] = 'Активная';
+$_lang['mxcalendars.feed_col_type'] = 'Тип';
+$_lang['mxcalendars.lastrunon_col_label'] = 'Последнее обновление';
+$_lang['mxcalendars.nextrunon_col_label'] = 'Следующее обновление';
+$_lang['mxcalendars.label_default'] = 'По умолчанию';
+$_lang['mxcalendars.source'] = 'Источник';
+$_lang['mxcalendars.label_feedmeasurementtime'] = 'Enter number of units';
+$_lang['mxcalendars.label_feedmeasurementtype'] = 'Тип таймера';
+
+$_lang['mxcalendars.feed_type_label_select_empty'] = '--выберите--';
+$_lang['mxcalendars.measurement_type_label_select_empty'] = '--выберите--';
+
+?>
