04ac22c2c6146844637a9a71f286f6dba82365f8
Sebastian G. Marinescu <sebastian.marinescu@gmail.com>
2019-01-19 22:17:12 +0100

    Formatting

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -5,16 +5,16 @@
  * @package mxCalendar
  * @subpackage build
  */
-$success= false;
+$success = false;
 
 if ($object->xpdo) {
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
 
         case xPDOTransport::ACTION_INSTALL:
             $modx =& $object->xpdo;
-            $modelPath = $modx->getOption('mxcalendars.core_path',null,$modx->getOption('core_path').'components/mxcalendars/').'model/';
-            $modx->addPackage('mxcalendars',$modelPath);
- 
+            $modelPath = $modx->getOption('mxcalendars.core_path', null, $modx->getOption('core_path') . 'components/mxcalendars/') . 'model/';
+            $modx->addPackage('mxcalendars', $modelPath);
+
             $m = $modx->getManager();
 
 
@@ -29,81 +29,81 @@ if ($object->xpdo) {
             $m->createObjectContainer('mxCalendarLog');
 
             $c = $modx->newQuery('mxCalendarCategories');
-            $count = $modx->getCount('mxCalendarCategories',$c);
-            if(!$count){
+            $count = $modx->getCount('mxCalendarCategories', $c);
+            if (!$count) {
                 $mxcalendar_cat = $modx->newObject('mxCalendarCategories');
-                $mxcalendar_cat->fromArray(array('name'=>$options['defaultCategoryName'],'isdefault'=>1,'active'=>1));
+                $mxcalendar_cat->fromArray(array('name' => $options['defaultCategoryName'], 'isdefault' => 1, 'active' => 1));
                 $mxcalendar_cat->save();
             }
-            
+
             $c = $modx->newQuery('mxCalendarCalendars');
-            $count = $modx->getCount('mxCalendarCalendars',$c);
-            if(!$count){
+            $count = $modx->getCount('mxCalendarCalendars', $c);
+            if (!$count) {
                 $mxcalendar = $modx->newObject('mxCalendarCalendars');
-                $mxcalendar->fromArray(array('name'=>$options['defaultCategoryName'], 'active'=>1));
+                $mxcalendar->fromArray(array('name' => $options['defaultCategoryName'], 'active' => 1));
                 $mxcalendar->save();
             }
             //-- ADD ANY ADDITIONAL PROPERTIES TO SET
-            if(isset($options['addDefaultCat']) && !empty($options['defaultCategoryName'])){
-                $d = $modx->getCount('mxCalendarCategories',array('isdefault' => 1,));
-                if($d <= 0){
+            if (isset($options['addDefaultCat']) && !empty($options['defaultCategoryName'])) {
+                $d = $modx->getCount('mxCalendarCategories', array('isdefault' => 1,));
+                if ($d <= 0) {
                     $setting = $modx->newObject('mxCalendarCategories');
-                    $setting->set('name',$options['defaultCategoryName']);
+                    $setting->set('name', $options['defaultCategoryName']);
                     $setting->set('isdefault', 1);
 
-                    if( $setting->save() ){
-                        $modx->log(xPDO::LOG_LEVEL_INFO, '[mxCalendar] default category <strong>:'.$options['defaultCategoryName'].'</strong> was created.');
+                    if ($setting->save()) {
+                        $modx->log(xPDO::LOG_LEVEL_INFO, '[mxCalendar] default category <strong>:' . $options['defaultCategoryName'] . '</strong> was created.');
                     } else {
-                        $modx->log(xPDO::LOG_LEVEL_ERROR,'[mxCalendar] default category could not be created');                
+                        $modx->log(xPDO::LOG_LEVEL_ERROR, '[mxCalendar] default category could not be created');
                     }
                 } else {
                     $modx->log(xPDO::LOG_LEVEL_INFO, '[mxCalendar] default category <strong> already set.');
                 }
             }
-            
+
             //-- Require Category
-            $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'mxcalendars.category_required'));
+            $setting = $object->xpdo->getObject('modSystemSetting', array('key' => 'mxcalendars.category_required'));
             if ($setting != null) {
-                $setting->set('value',$options['mxc-catreq']);
+                $setting->set('value', $options['mxc-catreq']);
                 $setting->save();
             } else {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'[mxCalendar] the category setting was not saved for the requirement setting.');
+                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR, '[mxCalendar] the category setting was not saved for the requirement setting.');
             }
 
             //-- Set the event description input field type (htmleditor|textarea)
-            $setting = $object->xpdo->getObject('modSystemSetting',array('key' => 'mxcalendars.event_desc_type'));
+            $setting = $object->xpdo->getObject('modSystemSetting', array('key' => 'mxcalendars.event_desc_type'));
             if ($setting != null) {
-                $setting->set('value',$options['mxc-desctype']);
+                $setting->set('value', $options['mxc-desctype']);
                 $setting->save();
             } else {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'[mxCalendar] was unable to set default input type for event description.');
+                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR, '[mxCalendar] was unable to set default input type for event description.');
             }
-            
-            $success= true;
+
+            $success = true;
             break;
-            
+
         case xPDOTransport::ACTION_UPGRADE:
-            
+
             $modx =& $object->xpdo;
-            $modelPath = $modx->getOption('mxcalendars.core_path',null,$modx->getOption('core_path').'components/mxcalendars/').'model/';
-            $modx->addPackage('mxcalendars',$modelPath);
-            
+            $modelPath = $modx->getOption('mxcalendars.core_path', null, $modx->getOption('core_path') . 'components/mxcalendars/') . 'model/';
+            $modx->addPackage('mxcalendars', $modelPath);
+
             $m = $modx->getManager();
 
             // Versions 1.1.7-pl through 1.1.9-pl updates
-            $m->alterField('mxCalendarEvents','categoryid');
-            $m->addField('mxCalendarEvents','allday');
+            $m->alterField('mxCalendarEvents', 'categoryid');
+            $m->addField('mxCalendarEvents', 'allday');
             $m->createObjectContainer('mxCalendarEventImages');
-            $m->addField('mxCalendarCategories','parent');
-            
+            $m->addField('mxCalendarCategories', 'parent');
+
             // Version 1.1.10-pl
             $m->createObjectContainer('mxCalendarEventVideos');
-            $m->addField('mxCalendarEvents','content');
-            
+            $m->addField('mxCalendarEvents', 'content');
+
             $success = true;
             break;
         case xPDOTransport::ACTION_UNINSTALL:
-            $success= true;
+            $success = true;
             break;
 
     }
